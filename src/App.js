89f8334995eb06178b89416a04f32928ca80b0db
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-08-25 08:30:22 -0700

    feat: add profilet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Posts from "./components/posts/Posts";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Profile from "./components/Profile";
 import {useState} from "react";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App() {
